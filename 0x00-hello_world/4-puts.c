59cd000c75700f2905e65cc206906bf74c96091d
Jeff <3374@holbertonschool.com>
2021-09-27 12:04:50 -0700

    Edit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
-int main(void)
+#include <stdio.h>
 /**
 *puts - using puts
-*puts should print "Pringramming is like
-*building a multilingual puzzle
+*
+*puts should print "Programming is like
+*
+*building a multilinguial puzzle"
 */
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
