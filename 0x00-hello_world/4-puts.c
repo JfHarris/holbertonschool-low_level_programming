5e3240d11bf9aec094bce2b5e70ebb5ed3e11215
Jeff <3374@holbertonschool.com>
2021-09-27 11:34:12 -0700

    Edit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
-int main()
+int main(void)
+/**
+*puts - using puts
+*puts should print "Pringramming is like
+*building a multilingual puzzle
+*/
 {
   char jeff[] = "Programming is like building a multilingual puzzle";
-  
   puts(jeff);
-
-  return 0;
+  return (0);
+  /**
+  *Return: 0
+  */
 }
