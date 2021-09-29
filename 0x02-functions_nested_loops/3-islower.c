fbc88a192c03a607b4707115ef5808078653af22
Jeff <3374@holbertonschool.com>
2021-09-29 15:01:40 -0700

    Edit 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower- returns 1
+ *@c: variable of int or char
+ * Return: c.
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+c = 1;
+}
+else
+{
+c = 0;
+}
+return (c);
+}
