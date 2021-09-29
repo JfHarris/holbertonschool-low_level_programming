df9a12088b8dcbac6a380944af878305e8e72ec7
Jeff <3374@holbertonschool.com>
2021-09-28 19:56:31 -0700

    Edit 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,9 +6,18 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 0 ; i <=9 ; i++)
-    {
-      putchar(i);
-    }
-  
+int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+putchar(i % 10 + '0');
+}
+for (i = 0 ; i <= 9 ; i++)
+{
+putchar(',');
+}
+for (i = 0 ; i <= 9 ; i++)
+{
+putchar(' ');
+}
+return (0);
+}
