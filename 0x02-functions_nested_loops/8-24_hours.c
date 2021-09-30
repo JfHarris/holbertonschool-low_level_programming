c352f94d46bb6e40c8ba3d25c979b1f10c57d863
Jeff <3374@holbertonschool.com>
2021-09-30 11:16:20 -0700

    Edit 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *jack_bauer - torture time
+ *
  *
  * Return: Always 0.
  */
 
 void jack_bauer(void)
 {
+int m;
+int h;
 
+for (h = 0 ; h <= 23 ; h++)
+{
+if (h < 10)
+{
+printf("0");
+}
+printf("%d:", h);
+for (m = 0 ; m <= 59 ; m++)
+if (m < 10)
+{
+printf("0");
+}
+printf("%d\n", m);
+}
 return;
 }
