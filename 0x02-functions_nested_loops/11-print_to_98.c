43b92ee1828c51da19e3422e4749e55b8e20beb1
Jeff <3374@holbertonschool.com>
2021-09-29 20:13:13 -0700

    Edit 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
  *print_to_98- print all natural numbers to 98
- *
+ *@n: var
  * Return: Always 0.
  */
 
 void print_to_98(int n)
 {
+for (n = 1 ; n <= 98 ; n++)
+{
+_putchar(n);
+}
+if (n != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
 return;
 }
