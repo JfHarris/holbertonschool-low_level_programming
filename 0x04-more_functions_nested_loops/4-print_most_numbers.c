60abe780cc220145e17e291d5eb3a39a1fc069fe
Jeff <3374@holbertonschool.com>
2021-10-03 14:57:13 -0700

    betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
 
 void print_most_numbers(void)
 {
-  int x;
+int x;
 
-  for (x = 0 ; x <= 9 ; x++)
-    {
-      if (x != 2 && x != 4)
-	_putchar(x + '0');
-    }
-  _putchar('\n');
+for (x = 0 ; x <= 9 ; x++)
+{
+if (x != 2 && x != 4)
+_putchar(x + '0');
+}
+_putchar('\n');
 
-  return;
+return;
 }
