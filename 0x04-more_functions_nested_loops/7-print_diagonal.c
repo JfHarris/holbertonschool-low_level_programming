4da30e8a713fc707e38d6d6ba054223c0e33cc01
Jeff <3374@holbertonschool.com>
2021-10-02 22:43:49 -0700

    Edit 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,4 +7,18 @@
 
 void print_diagonal(int n)
 {
+  int x;
+  int y;
+  char a = '\\'
+
+    for (x = 0 ; x < n ; x++)
+      {
+       for (y = 0 ; y <= x ; y++)
+	 {
+	  _putchar(a);
+	  _putchar('\n');
+	 }
+      }
+  _putchar('\n');
+  return;
 }
