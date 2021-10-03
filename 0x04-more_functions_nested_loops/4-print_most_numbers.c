e4297683def4e1f7d35b7e6680732c9fcdfc96d4
Jeff <3374@holbertonschool.com>
2021-10-03 14:55:35 -0700

    compiling

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
+ *print_most_numbers - skipping 2 and 4
  *
  * Return: Always 0.
  */
@@ -12,9 +12,9 @@ void print_most_numbers(void)
   for (x = 0 ; x <= 9 ; x++)
     {
       if (x != 2 && x != 4)
-	putchar(x + '0');
+	_putchar(x + '0');
     }
-  putchar('\n');
+  _putchar('\n');
 
-  return (0);
+  return;
 }
