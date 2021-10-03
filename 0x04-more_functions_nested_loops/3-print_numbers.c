af7a3cc8a5940ab9a3ffa53d3ee347643a02ef3d
Jeff <3374@holbertonschool.com>
2021-10-02 21:37:11 -0700

    Edit 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+  int x;
+
+  for (x >= 0 ; x <= 9; x++)
+    {
+    _putchar(x + '0');
+  _putchar(\n);
+    }
+}
