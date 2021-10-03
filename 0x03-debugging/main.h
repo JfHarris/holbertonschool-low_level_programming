ecadb47b9dcfe114d821e494cbe1e86d5bfb93ef
Jeff <3374@holbertonschool.com>
2021-10-03 13:27:11 -0700

    Edit main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,14 @@
-postitive_or_negative();
-print_remaining_days();
-int largest_number(int);
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+
+void postitive_or_negative(int x);
+
+void print_remaining_days(int month, int day, int year);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
