a30ac5d6463ad028d033bc1e5c01fece21f8969c
Jeff <3374@holbertonschool.com>
2021-10-02 22:06:49 -0700

    Edit 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,4 +7,19 @@
 
 void more_numbers(void)
 {
+  int x;
+  int a;
+
+  for (x = 0 ; x <= 9 ; x++)
+    {
+      for (a = 0 ; a <= 14 ; a++)
+	{
+	  _putchar(a + '0');
+	}
+      if (a > 14)
+	{
+	  _putchar('\n');
+	}
+    }
+  return;
 }
