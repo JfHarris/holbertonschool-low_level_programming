d21fe91414afed895cc24456382532ab1c050f99
Jeff <3374@holbertonschool.com>
2021-10-03 15:21:07 -0700

    betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- *print_lines - printing blank lines
- *
+ *print_line - printing blank lines
+ *@n: variable
  * Return: Always 0.
  */
 
 void print_line(int n)
 {
-  int x;
+int x;
 
-  if (n > 0)
-    {
-      for (x = 0 ; x < n ; x++)
-	{
-	_putchar(' ');
-	}
-    }
-  _putchar('\n');
-  return;
+if (n > 0)
+{
+for (x = 0 ; x < n ; x++)
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+return;
 }
