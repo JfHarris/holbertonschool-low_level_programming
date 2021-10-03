eb1597cdf3f95be48aaaf5ee112f1a9572a320bd
Jeff <3374@holbertonschool.com>
2021-10-03 14:42:24 -0700

    Edit 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ *mul - multiply int
+ *@a: var
+ *@b: var
  * Return: Always 0.
  */
 
 int mul(int a, int b)
 {
-  int a;
-  int b;
-  int c;
+int c;
 
-  if (c = a * b)
-    {
-      _putchar(c + '0');
-    }
-  return (0);
+c = a * b;
+
+return (c);
 }
