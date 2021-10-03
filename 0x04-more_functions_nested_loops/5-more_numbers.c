3a9ef600740d966d6c17784a5fb0079eba8179b6
Jeff <3374@holbertonschool.com>
2021-10-03 15:00:52 -0700

    betty

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * main - check the code
+ *more_numbers - printing 0-14 ten times
  *
  * Return: Always 0.
  */
 
 void more_numbers(void)
 {
-  int x;
-  int a;
+int x;
+int a;
 
-  for (x = 0 ; x <= 9 ; x++)
-    {
-      for (a = 0 ; a <= 14 ; a++)
-	{
-	  _putchar(a + '0');
-	}
-      if (a > 14)
-	{
-	  _putchar('\n');
-	}
-    }
-  return;
+for (x = 0 ; x <= 9 ; x++)
+{
+for (a = 0 ; a <= 14 ; a++)
+{
+_putchar(a + '0');
+}
+if (a > 14)
+{
+_putchar('\n');
+}
+}
+return;
 }
