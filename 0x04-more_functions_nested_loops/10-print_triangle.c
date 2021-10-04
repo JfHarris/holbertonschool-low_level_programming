86c9d040b9cf1bda9db9d589110a63dbe6f9839b
Jeff <3374@holbertonschool.com>
2021-10-04 08:53:41 -0700

    Edit 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,4 +7,23 @@
 
 void print_triangle(int size)
 {
+  int x;
+  int y;
+  
+  if (size > 0)
+    {
+      for (x = 0 ; x < size ; x++)
+	{
+	  for (y = 0 ; y < size ; y++)
+	    {
+	      _putchar('#');
+	    }
+	  _putchar('\n');
+	}
+    }
+  else
+	{
+	  _putchar('\n');
+	}
+return;
 }
