ed77eb724544025cba57a9fb0f7fc37d3d29602e
Jeff <3374@holbertonschool.com>
2021-10-04 20:17:46 -0700

    Edit 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,15 @@ int main(void)
 
  for (i = 1; i <= 100; i++)
    {
-     printf(" %d", i);
      if (i % 3 == 0  && i % 5 == 0)
        {
 	 printf(" FizzBuzz");
        }
-     if (i % 3 == 0)
+     else if (i % 3 == 0)
        {
 	 printf(" Fizz");
        }
-     if (i % 5 == 0)
+     else if (i % 5 == 0)
        {
 	 printf(" Buzz");
        }
