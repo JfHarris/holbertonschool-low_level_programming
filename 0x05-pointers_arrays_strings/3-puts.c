410afa2a4e0764e506481699d52beb0e646a7ca7
Jeff <3374@holbertonschool.com>
2021-10-05 11:16:06 -0700

    Edit 3/betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *puts- print string, new line, to stdout
+ *_puts- print string, new line, to stdout
  *@str: pointer
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-  int x;
+int x;
 
-  for (x = 0 ; x <= 66 ; x++)
-    {
-      str++;
-      _putchar(*str);
-      _putchar('\n');
-    }
-  return;
+for (x = 0 ; x <= 66 ; x++)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+return;
 }
