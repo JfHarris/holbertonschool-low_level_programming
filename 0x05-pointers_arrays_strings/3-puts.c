4d4550ed71436d2ca8df6de08e20cf2e84f69f5f
Jeff <3374@holbertonschool.com>
2021-10-05 10:49:16 -0700

    Edit 3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  *puts- print string, new line, to stdout
- *
+ *@str: pointer
  * Return: Always 0.
  */
 void _puts(char *str)
 {
+  _putchar(*str);
+  _putchar('\n');
   return;
 }
