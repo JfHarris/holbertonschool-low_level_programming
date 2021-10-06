d0dcfa2a36b11b44248b22b48d23986f4c346abf
Jeff <3374@holbertonschool.com>
2021-10-06 11:34:35 -0700

    betty/compiling

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,23 @@
 #include "main.h"
 /**
  *puts2- print every other character then new line
- *
+ *@str: pointer
  * Return: Always 0.
  */
 void puts2(char *str)
 {
-  return;
+int x;
+int y;
+
+x = 0;
+for (y = 0 ; str[y] != 0 ; y++)
+{
+x++;
+}
+for (y = 0 ; y < x ; y += 2)
+{
+_putchar(str[y]);
+}
+_putchar('\n');
+return;
 }
