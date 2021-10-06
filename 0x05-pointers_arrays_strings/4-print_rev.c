5a14b33875f228e13729b91db6b42084e0752e6a
Jeff <3374@holbertonschool.com>
2021-10-06 08:03:44 -0700

    betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,17 @@
  */
 void print_rev(char *s)
 {
-  while (*s != 0)
-    {
-      s++;
-    }
-  s--;
-  while (*s != 0)
-    {
-      _putchar(*s);
-      s--;
-    }
-  _putchar('\n');
+while (*s != 0)
+{
+s++;
+}
+s--;
+while (*s != 0)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
 
 return;
 }
