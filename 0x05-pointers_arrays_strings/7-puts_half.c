a45038d3192adc9ba677c6fae3b4bfed88cb45e6
Jeff <3374@holbertonschool.com>
2021-10-06 13:45:32 -0700

    betty/compiling

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,35 @@
 #include "main.h"
 /**
  *puts_half- print half a string then new line
- *
+ *@str: str
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
-  return;
+int x;
+int y;
+
+x = 0;
+
+for (y = 0 ; str[y] ; y++)
+{
+x++;
+}
+if (x % 2 == 0)
+{
+for (y = x / 2 ; y < x ; y++)
+{
+_putchar(str[y]);
+}
+}
+else
+{
+for (y = (x / 2) + 1 ; y < x ; y++)
+{
+_putchar(str[y]);
+}
+}
+_putchar('\n');
+
+return;
 }
