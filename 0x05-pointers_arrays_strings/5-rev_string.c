233ae6dc372457e9b9e25b1ebd696ddae07f856a
Jeff <3374@holbertonschool.com>
2021-10-06 20:06:17 -0700

    compiling

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,23 @@
  */
 void rev_string(char *s)
 {
+  int x;
+  int y;
+  char a;
+
+  x = 0;
+  y = 1;
+
+  while (s[x++])
+    {
+      y++;
+    }
+  for (x = y - 1 ; x >= (y / 2) ; x--)
+    {
+      a = s[x];
+      s[x] = s[y - x - 1];
+      s[y - x - 1] = a;
+     
+    }
   return;
 }
