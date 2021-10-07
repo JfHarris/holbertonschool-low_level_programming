8e9aad40b612453a7a95e4ba3df833b432881b8d
Jeff <3374@holbertonschool.com>
2021-10-07 11:13:59 -0700

    Edit 9/compiling

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,19 @@
 #include "main.h"
 /**
  *_strcpy- copy string from src to dest
- *
+ *@dest: char
+ *@src: char
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
-  return (0);
+  int x;
+
+  for (x = 0 ; src[x] != 0 ; x++)
+    {
+      dest[x] = src[x];
+    }
+  dest[x++] = '\0';
+
+  return(dest);
 }
