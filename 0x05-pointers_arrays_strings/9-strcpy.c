e269676818dfb6032b5d272d97cc04f356ae1498
Jeff <3374@holbertonschool.com>
2021-10-07 11:15:39 -0700

    betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,13 +7,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  int x;
+int x;
 
-  for (x = 0 ; src[x] != 0 ; x++)
-    {
-      dest[x] = src[x];
-    }
-  dest[x++] = '\0';
+for (x = 0 ; src[x] != 0 ; x++)
+{
+dest[x] = src[x];
+}
+dest[x++] = '\0';
 
-  return(dest);
+return (dest);
 }
