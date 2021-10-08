445bea9cba82822859f86a0617fb1509a1e0b803
Jeff <3374@holbertonschool.com>
2021-10-08 14:29:17 -0700

    betty/compiling

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,5 +9,24 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  return;
+int x;
+int y;
+
+x = 0;
+y = 0;
+
+while (dest[x] != '\0')
+{
+x++;
+}
+while (y < n && src[y] != '\0')
+{
+dest[x] = src[y];
+x++;
+y++;
+}
+
+dest[x] = '\0';
+
+return (dest);
 }
