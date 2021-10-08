33744c1f06c6207ae342065c54f78c5944881ff1
Jeff <3374@holbertonschool.com>
2021-10-08 15:01:00 -0700

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,5 +9,20 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  return;
+int x;
+
+x = 0;
+
+while (x < n && src[x] != '\0')
+{
+dest[x] = src[x];
+x++;
+}
+while (x < n)
+{
+dest[x] = '\0';
+x++;
+}
+
+return (dest);
 }
