97d97544b3a113c8199233dd77580e316ddf4d0d
Jeff <3374@holbertonschool.com>
2021-10-08 17:52:17 -0700

    Edit 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,11 +3,23 @@
 
 /**
  *string_toupper - change all lowercase letters of a string to uppercase
- *
+ *@a: pointer to string
  * Return: Always 0.
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *a)
 {
-  return (0);
+int x;
+
+x = 0;
+
+ for (x = 0 ; a[x] != '\0' ; x++)
+   {
+     if (a[x] >= 'a' && a[x] <= 'z')
+       {
+	 a[x] = a[x] - 32;
+       }
+   }
+
+return (a);
 }
