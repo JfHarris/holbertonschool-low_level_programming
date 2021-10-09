0b7e2e687309f9b022891b3877d7921b5d7c4694
Jeff <3374@holbertonschool.com>
2021-10-08 22:05:02 -0700

    Edit 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,127 @@
 
 /**
  *cap_string - capitalize all words of a string.
- *
+ *@a: pointer
  * Return: Always 0.
  */
-char *cap_string(char *)
+char *cap_string(char *a)
 {
-  return;
-}
+  int x;
+
+  for (x = 0 ; a[x] != '\0' ; x++)
+    {
+      if (x == 0)
+	{
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == ' ')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      else if (a[x] == '\t')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      else if (a[x] == '\n')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == ',')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == ';')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == '.')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == '!')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == '?')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+      if (a[x] == '\"')
+	{
+	  ++x;
+	  if (a[x] >= 'a' && a[x] <= 'z')
+	    {
+	      a[x] = a[x] - 32;
+	    }
+	}
+	if (a[x] == '(')
+	  {
+	    ++x;
+	    if (a[x] >= 'a' && a[x] <= 'z')
+	      {
+		a[x] = a[x] - 32;
+	      }
+	  }
+	  if (a[x] == ')')
+	    {
+	      ++x;
+	      if (a[x] >= 'a' && a[x] <= 'z')
+		{
+		  a[x] = a[x] - 32;
+		}
+	    }
+	    if (a[x] == '{')
+	      {
+		++x;
+		if (a[x] >= 'a' && a[x] <= 'z')
+		  {
+		    a[x] = a[x] - 32;
+		  }
+	      }
+	      if (a[x] == '}')
+		{
+		  ++x;
+		  if (a[x] >= 'a' && a[x] <= 'z')
+		    {
+		      a[x] = a[x] - 32;
+		    }
+		}
+		}
+	      return (a);
+	      }
+
