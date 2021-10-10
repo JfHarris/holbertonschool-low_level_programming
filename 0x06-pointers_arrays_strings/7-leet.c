82b2f0fd529ccccb066b24d5f197fe04e90e7358
Jeff <3374@holbertonschool.com>
2021-10-09 20:28:49 -0700

    betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,8 +2,8 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ *leet - switching certain chars for ints
+ *@s: pointer
  * Return: Always 0.
  */
 
@@ -12,18 +12,18 @@ char *leet(char *s)
 
 int x;
 int y;
-char i[] = {'7' , '7' , '4' , '4' , '3' , '3' , '1' , '1', '0' , '0'};
-char a[] = {'T' , 't' , 'A' , 'a' , 'E' , 'e' , 'L' , 'l' , 'O' , 'o'};
+char i[] = {'7', '7', '4', '4', '3', '3', '1', '1', '0', '0'};
+char a[] = {'T', 't', 'A', 'a', 'E', 'e', 'L', 'l', 'O', 'o'};
 
 for (x = 0 ; s[x] != 0 ; x++)
 {
-  for (y = 0 ; y <= 9 ; y++)
-  {
-    if (s[x] == a[y])
-    {
-      s[x] = i[y];
-    }
-  }
+for (y = 0 ; y <= 9 ; y++)
+{
+if (s[x] == a[y])
+{
+s[x] = i[y];
+}
+}
 }
 return (s);
 }
