ea2f172bf72f31b4ddd6633f3163ed422a3d9941
Jeff <3374@holbertonschool.com>
2021-10-09 20:25:28 -0700

    compile

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,23 @@
  * Return: Always 0.
  */
 
-char *leet(char *)
+char *leet(char *s)
 {
-  return;
+
+int x;
+int y;
+char i[] = {'7' , '7' , '4' , '4' , '3' , '3' , '1' , '1', '0' , '0'};
+char a[] = {'T' , 't' , 'A' , 'a' , 'E' , 'e' , 'L' , 'l' , 'O' , 'o'};
+
+for (x = 0 ; s[x] != 0 ; x++)
+{
+  for (y = 0 ; y <= 9 ; y++)
+  {
+    if (s[x] == a[y])
+    {
+      s[x] = i[y];
+    }
+  }
+}
+return (s);
 }
