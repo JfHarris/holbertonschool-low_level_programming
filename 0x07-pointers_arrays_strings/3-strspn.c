32826fea10d6b1cfa497f97362236b5752ba55bb
Jeff <3374@holbertonschool.com>
2021-10-11 12:18:33 -0700

    betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,29 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ *_strspn - returns length of substring
+ *@s: target string
+ *@accept: receives from s
  * Return: Always 0.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-  return;
+unsigned int x;
+unsigned int y;
+
+for (x = 0 ; s[x] != '\0' ; x++)
+{
+for (y = 0 ; accept[y] != '\0' ; y++)
+{
+if (accept[y] == s[x])
+{
+break;
+}
+}
+if (accept[y] == '\0')
+{
+return (x);
+}
+}
+return (x);
 }
