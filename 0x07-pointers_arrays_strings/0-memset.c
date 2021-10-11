0d3953d943673437dfe228ee3f62e74e7363b756
Jeff <3374@holbertonschool.com>
2021-10-11 09:08:45 -0700

    Add/edit 0 to 8 c files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  return;
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ *strchr - locates character in a string
+ *@s: pointer
+ *@c: char to be found
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+  return;
+}
