711d876198006662da183827cb364744a4944354
Jeff <3374@holbertonschool.com>
2021-10-12 12:39:14 -0700

    betty/compile

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen_recursion - finds string length
+ *@s: string
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(++s));
+}
