9ec1b04d50071a0b4f211e8b3f4beb152ff79c3f
Jeff <3374@holbertonschool.com>
2021-10-12 11:57:19 -0700

    betty/compile

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - printing reverse
+ *@s: string
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+else
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
