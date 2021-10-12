607fc674f91fc2a34ec4ddfeddbce4e6c3ee1432
Jeff <3374@holbertonschool.com>
2021-10-11 19:41:38 -0700

    compile

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,5 +7,19 @@
 
 void print_chessboard(char (*a)[8])
 {
+  int hor;
+  int vert;
+
+  hor = 0;
+  vert = 0;
+
+  for (hor = 0 ; hor < 8 ; hor++)
+    {
+      for (vert = 0 ; vert < 8 ; vert++)
+	{
+	  _putchar(a[hor][vert]);
+	}
+      _putchar('\n');
+    }
   return;
 }
