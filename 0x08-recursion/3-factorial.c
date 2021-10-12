f78e794121f71601b042d376cad0cc63765ef7b2
Jeff <3374@holbertonschool.com>
2021-10-12 16:38:33 -0700

    betty/compile

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *factorial - factorial
+ *@n: number
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 1 || n == 0)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n - 1));
+}
+}
