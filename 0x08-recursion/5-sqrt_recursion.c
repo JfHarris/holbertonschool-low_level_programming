819d796bc4390497b0c32744631d0cacea281293
Jeff <3374@holbertonschool.com>
2021-10-13 14:15:31 -0700

    edit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *findsqr - find square root
+ *@n: int
+ *@x: value of n
+ * Return: square root or -1.
+ */
+
+int findsqr(int n, int x)
+{
+	if (x * x == n)
+	{
+		return (x);
+	}
+
+	if (x * x > n)
+	{
+		return (-1);
+	}
+
+	return (findsqr(n, x + 1));
+}
+
+/**
+ *_sqrt_recursion - square root
+ *@n: int
+ *
+ * Return: square root or -1.
+ */
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	x = 1;
+
+	return (findsqr(n, x));
+}
