3afaa8becaafb704d0833d2f21910b14eba117b1
Jeff <3374@holbertonschool.com>
2021-10-13 13:49:53 -0700

    betty/compile

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * main - check the code
- *
+ *print_diagsums - print sums of nums diagonal to each other
+ *@a: pointer
+ *@size: size of arrays
  * Return: Always 0.
  */
 
 void print_diagsums(int *a, int size)
 {
-  return;
+	int x;
+	int sum;
+	int sumb;
+	int *b;
+
+	b = a;
+	sum = 0;
+	sumb = 0;
+
+	for (x = 0 ; x < size ; x++)
+	{
+		sum = sum + a[x];
+		a = a + size;
+	}
+	printf("%d, ", sum);
+
+	for (x = size - 1; x >= 0 ; x--)
+	{
+		sumb = sumb + b[x];
+		b = b + size;
+	}
+	printf("%d", sumb);
+	printf("\n");
 }
