a6ebfcb9d0d754a53cba4bc142435cca72062c2e
Jeff <3374@holbertonschool.com>
2021-10-12 17:49:15 -0700

    betty

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_pow_recursion - x power of y
+ *@x: number
+ *@y: number
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
+}
+else if (y > 0)
+{
+return (x * _pow_recursion(x, y - 1));
+}
+else
+{
+return (-1);
+}
+}
