db58f6d18944df9caf8ae5211ec37a4c565058a9
Jeff <3374@holbertonschool.com>
2021-10-13 11:52:01 -0700

    edit/compile/betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,28 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * main - check the code
- *
+ *_strstr - finds matching bytes of substring in string
+ *@haystack: string to check
+ *@needle: substring to find
  * Return: Always 0.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-  return;
+int i;
+
+if (!*needle)
+{
+	return (haystack);
+}
+while (*haystack)
+{
+	for (i = 0 ; haystack[i] == needle[i] ; ++i)
+	{
+		if (!needle[i + 1])
+			return (haystack);
+	}
+	++haystack;
+}
+return (NULL);
 }
