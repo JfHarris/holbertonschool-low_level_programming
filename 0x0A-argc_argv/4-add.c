42222210262bcdec020bf52689dadeb08b6493d2
Jeff <3374@holbertonschool.com>
2021-10-16 13:27:30 -0700

    betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *main - adding numbers
+ *@argc: numbers to add
+ *@argv: array
  *
- *
- *
+ *Return: if not digit (1) if no digit (0) else (sum)
  */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int sum;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (x = 1 ; x < argc ; x++)
+	{
+		y = atoi(argv[x]);
+
+		if (y == '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum = sum + y;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
