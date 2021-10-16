bdaa04e58167f2ccbc003f976bf5bf08bf8cab92
Jeff <3374@holbertonschool.com>
2021-10-16 13:03:12 -0700

    betty

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *main - multiply two arguments
+ *@argc: numbers to multiply
+ *@argv: array
  *
- *
- *
+ *Return: 0
  */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+
+	if (argc >= 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", (x * y));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
