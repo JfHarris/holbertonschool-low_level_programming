a8402337777fb69ee27a5420ef884d81e4e5bd47
Jeff <3374@holbertonschool.com>
2021-10-15 21:25:16 -0700

    Edit/betty/compile 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *main - print number of arguments
+ *@argc: number of arguments
+ *@argv: unused attribute
  *
- *
- *
+ *Return: (0)
  */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
