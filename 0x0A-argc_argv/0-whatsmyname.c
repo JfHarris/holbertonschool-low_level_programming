c7b008f03ab0756187fe45e75d2a64ffe02a95a3
Jeff <3374@holbertonschool.com>
2021-10-15 21:02:55 -0700

    betty/compile 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ *main - print name of function
+ *@argc: arhument count
+ *@argv: name to print
  *
- *
+ *Return: (0)
  */
 
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
