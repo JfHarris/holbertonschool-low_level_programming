fb73bcdeb3ae67be1fbebf3b6dac9da337e85402
Jeff <3374@holbertonschool.com>
2021-10-15 21:38:11 -0700

    Edit/betty/compile 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *main - print all arguments
+ *@argc: number of args
+ *@argv: string containing args
  *
- *
- *
+ *Return: 0
  */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0 ; x < argc ; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+return (0);
+}
