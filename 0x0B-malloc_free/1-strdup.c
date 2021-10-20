7e615666516db2b15ea481886c53af56e7b3840a
Jeff <3374@holbertonschool.com>
2021-10-20 13:28:44 -0700

    betty

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strdup - return pointer to space in memory
+ *@str: string to copy
+ * Return: pointer mentioned above
+ */
+
+char *_strdup(char *str)
+{
+	int x;
+	int y;
+	char *a;
+
+	y = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[y] != '\0')
+	{
+		y++;
+	}
+
+	a = malloc(y * sizeof(char));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0 ; x < y ; x++)
+	{
+		a[x] = str[x];
+	}
+
+	return (a);
+
+}
