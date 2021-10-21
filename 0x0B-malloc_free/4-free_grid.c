f9676dc2df414b674b69805f02663248dec46451
Jeff <3374@holbertonschool.com>
2021-10-20 19:40:57 -0700

    betty/compile

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ *
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0 ; x < height ; x++)
+	{
+		free(grid[x]);
+	}
+
+	free(grid);
+}
