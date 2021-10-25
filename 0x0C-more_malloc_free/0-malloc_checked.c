76ebed5334344c2f3f8c2e0a4293bf35050db3df
JfHarris <3374@holbertonschool.com>
2021-10-25 12:14:49 -0700

    Edit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - check success of malloc
+ *@b: pointer
+ * Return: Always 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (x == NULL)
+	{
+		exit(98);
+	}
+
+	return (x);
+}
