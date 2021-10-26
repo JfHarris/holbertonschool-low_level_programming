92806d1ed921c7fde1dc5280728df4f5ca814174
JfHarris <3374@holbertonschool.com>
2021-10-26 04:01:10 -0700

    Add/edit dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *struct dog - details about dog
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
