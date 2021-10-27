e3490b9a8b49043983b5a7d9f36fa6fbc12d9286
JfHarris <3374@holbertonschool.com>
2021-10-27 14:43:09 -0700

    Edited for loop

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,13 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if (array == NULL && action == NULL)
-	{
-		return;
-	}
+	if (array != NULL && action != NULL)
 
 	for (x = 0 ; x < size ; x++)
 	{
-		action(array[x]);
+		action(*array);
+		array++;
 	}
 }
