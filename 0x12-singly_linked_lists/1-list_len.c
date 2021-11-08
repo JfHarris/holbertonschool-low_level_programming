25001ac2b5de768b818ecedc69cb77d5ad792474
JfHarris <3374@holbertonschool.com>
2021-11-08 14:14:18 -0800

    Add/edit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - find length of list
+ *@h: pointer
+ *
+ * Return: nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	if (h->next != NULL)
+	{
+		return (list_len(h->next) + 1);
+	}
+
+	return (1);
+}
