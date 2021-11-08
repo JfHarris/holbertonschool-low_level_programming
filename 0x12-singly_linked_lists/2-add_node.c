ae3f9418668265b32aa29f361e41f9163913187e
JfHarris <3374@holbertonschool.com>
2021-11-08 15:04:49 -0800

    Add/edit 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - add node to list
+ *@head: start of string
+ *@str: string
+ *
+ * Return: nodes.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
