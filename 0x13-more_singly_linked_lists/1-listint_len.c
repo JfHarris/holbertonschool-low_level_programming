c327c1a161cf03fee9d288173f47072fcaec835e
JfHarris <3374@holbertonschool.com>
2021-11-10 14:52:35 -0800

    Add/edit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - find length of list
+ *@h: pointer
+ *
+ * Return: nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	if (h->next != NULL)
+	{
+		return (listint_len(h->next) + 1);
+	}
+
+	return (1);
+}
