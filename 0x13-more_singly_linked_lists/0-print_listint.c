7219c7533161a30ea6d44aba7572cb6389719970
JfHarris <3374@holbertonschool.com>
2021-11-10 14:33:40 -0800

    Add/edit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - print linked list
+ *@h: pointer
+ *
+ * Return: nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
