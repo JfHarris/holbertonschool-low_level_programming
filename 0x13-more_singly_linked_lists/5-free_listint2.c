b96163d1780f717e4a724b2d29461cdb7966f95e
JfHarris <3374@holbertonschool.com>
2021-11-11 12:42:41 -0800

    Add/edit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list memory
+ *@head: first node
+ *
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *store;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	temp = *head;
+	while (temp != NULL)
+	{
+		store = temp->next;
+		free(temp);
+		temp = store;
+	}
+
+	*head = NULL;
+}
