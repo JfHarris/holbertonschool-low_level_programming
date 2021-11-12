6a5781f51358a9505bdeaa408507399cb0e58028
JfHarris <3374@holbertonschool.com>
2021-11-12 15:46:00 -0800

    Add/edit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary
+ *@n: binary to print
+ *
+ * Return: void.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int x = 1ul << 63;
+	char c = x;
+
+	while (!(x & n) && x != 0)
+	{
+		x = x >> 1;
+	}
+
+	if (x == 0)
+	{
+		_putchar('0');
+	}
+
+	while (x)
+	{
+		if (x & n)
+		{
+			c = '1';
+		}
+		else
+		{
+			c = '0';
+		}
+		_putchar(c);
+		x = x >> 1;
+	}
+}
