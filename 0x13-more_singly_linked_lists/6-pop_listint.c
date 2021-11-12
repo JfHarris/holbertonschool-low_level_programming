e3c7cfaa5eec8a18b8d7886f1410b9d345480736
JfHarris <3374@holbertonschool.com>
2021-11-11 17:28:15 -0800

    Add/edit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete node
+ *@head: first node
+ *
+ * Return: head node data.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int x;
+
+	x = 0;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	temp = *head;
+	x = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return (x);
+}
