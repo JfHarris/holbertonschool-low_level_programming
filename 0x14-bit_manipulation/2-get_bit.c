ad6cdbe324d0a15c6612e4992b144c18ebf15983
JfHarris <3374@holbertonschool.com>
2021-11-12 17:16:41 -0800

    Add/Edit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - cet value
+ *@n: number to get
+ *@index: location of bit
+ *
+ * Return: bit value, or -1 on failure.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int x;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	x = 1 << index;
+	return ((n & x) > 0);
+}
