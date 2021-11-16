4e576d5ca45b63346e6893f3efc931939ca0699e
JfHarris <3374@holbertonschool.com>
2021-11-15 16:57:13 -0800

    Add/edit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit value
+ *@n: number to set
+ *@index: location of bit
+ *
+ * Return: bit value, or -1 on failure.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	*n = *n | 1ul << index;
+
+	return (1);
+}
