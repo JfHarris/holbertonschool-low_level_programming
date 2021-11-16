0ac08be6e1365e5ff0e053a934ec0d9179e7679c
JfHarris <3374@holbertonschool.com>
2021-11-15 20:16:44 -0800

    Add/edit

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get node at spot in list
+ *@head: first node
+ *@index: point to get node
+ * Return: Node. NULL if node is missing.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x = 0;
+
+	if (index == 0)
+	{
+		return (NULL);
+	}
+
+	while (x < index)
+	{
+		if (head->next == NULL)
+		{
+			return (NULL);
+		}
+		head = head->next;
+		x++;
+	}
+
+	return (head);
+}
