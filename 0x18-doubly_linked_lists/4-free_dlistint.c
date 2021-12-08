529ef826ed1313d7cc288b443afa23a202029f32
JfHarris <3374@holbertonschool.com>
2021-12-08 12:55:43 -0800

    Add/edit

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free list memory
+ *@head: pointer to head node
+ *
+ * Return: void.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *hold;
+
+	while (head != NULL)
+	{
+		hold = head->next;
+		free(head);
+		head = hold;
+	}
+}
