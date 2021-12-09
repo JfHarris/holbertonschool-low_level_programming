2460d94395f34e6d108a02a80afaf7129283909e
JfHarris <3374@holbertonschool.com>
2021-12-08 19:12:16 -0800

    Checking for check

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - prints elements of list backwards
+ *@h: pointer to head node
+ *
+ * Return: elements and contents of node.
+ */
+
+size_t print_dlistint_backward(const dlistint_t *h __attribute__((unused)))
+{
+	printf("Oops\n");
+	return (0);
+}
