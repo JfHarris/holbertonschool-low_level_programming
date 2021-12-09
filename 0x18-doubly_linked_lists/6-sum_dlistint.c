a74b6112c6d44ad846591a66a5cc2e858db6a186
JfHarris <3374@holbertonschool.com>
2021-12-08 18:45:34 -0800

    Add/edit

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns sum of all data in list
+ *@head: pointer to head node
+ *
+ * Return: sum.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int total;
+	dlistint_t *hold = head;
+
+	total = 0;
+
+	while (hold != NULL)
+	{
+		total += hold->n;
+		hold = hold->next;
+	}
+	return (total);
+}
