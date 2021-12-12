05d8db248c52d445627ca44000d77f97cc329638
JfHarris <3374@holbertonschool.com>
2021-12-12 15:56:30 -0800

    Add/edit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value of key
+ *@ht: hash table
+ *@key: key
+ *
+ * Return: value of key or NULL.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *site;
+	unsigned long int find;
+
+	if (!key || !ht)
+	{
+		return (NULL);
+	}
+
+	find = key_index((const unsigned char *)key, ht->size);
+
+	site = ht->array[find];
+	while (site)
+	{
+		if (strcmp((site->key), key) == 0)
+		{
+			return (site->value);
+		}
+		site = site->next;
+	}
+	return (NULL);
+}
