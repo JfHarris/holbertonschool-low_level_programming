faa4c570f5efa7e7a85acbb2ae7465f53f0f0f3f
JFHarris <JFHarris1983@gmail.com>
2022-06-14 17:22:32 -0500

    Compiles

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,7 +9,31 @@
  * else, If value is not present or if array is NULL return -1
  */
 
-int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value)
 {
+    size_t x;
+
+    x = 0;
+
+    if (array == NULL)
+    {
+        return -1;
+    }
+
+    for ( ; x < size; x++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+        if (array[x] == value)
+        {
+            break;
+        }
+    }
+
+    if (x == size)
+    {
+        return (x);
+    }
+
+    return (x);
 
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,7 +8,57 @@
  * Return: If value is not present in array or array is NULL return -1
  */
 
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value)
 {
+    size_t x;
+    size_t max;
+    size_t min;
+    size_t mid;
+
+    min = 0;
+    max = size - 1;
+    mid = 0;
+
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
+    while (min <= max)
+    {
+        mid = (max + min) / 2;
+        x = min;
+
+        printf("Searching in array: ");
+
+        for ( ; x <= max; x++)
+        {
+            if (x != min)
+            {
+                printf(", %d", array[x]);
+            }
+            else
+            {
+                printf("%d", array[x]);
+            }
+        }
+
+        printf("\n");
+
+        if (array[mid] < value)
+        {
+            min = mid + 1;
+        }
+        else if (array[mid] > value)
+        {
+            max = mid - 1;
+        }
+        else
+        {
+            return (mid);
+        }
+    }
+
+    return (-1);
 
 }
