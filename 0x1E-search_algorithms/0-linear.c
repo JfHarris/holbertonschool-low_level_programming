ab01717835f4febb9d1690f8eb0b0fd7e9698c49
JFHarris <JFHarris1983@gmail.com>
2022-06-13 22:55:25 -0500

    File/Directory creation. README added

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a val in array of ints
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value);
+{
+
+}
