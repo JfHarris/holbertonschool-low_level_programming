478e48cde27cd27713fb963ae1be736fcb8e987d
JFHarris <JFHarris1983@gmail.com>
2022-08-03 15:26:17 -0500

    Improving comment format

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i;
-for (i = 0 ; i <= 9 ; i++)
-{
-putchar(i % 10 + '0');
-}
-putchar('\n');
-return (0);
+    int i;
+    for (i = 0 ; i <= 9 ; i++)
+    {
+        putchar(i % 10 + '0');
+    }
+        putchar('\n');
+        return (0);
 }
